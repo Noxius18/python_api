926feb0785c34d38589debb8909a0870ce1a3f19
Noxius18 <akagiadam@gmail.com>
2024-11-25 21:17:43 +0700

    POST function

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import SQLAlchemyError
 from flask_restful import Resource, reqparse, fields, marshal_with, abort
 
 db = SQLAlchemy()
@@ -19,6 +20,7 @@ class BukuModel(db.Model):
             Genre = {self.genre},
             Tahun = {self.tahun})"""
 
+# Argument Parser untuk Valisasi Input (POST)
 buku_args = reqparse.RequestParser()
 buku_args.add_argument('judul', type=str, required=True, help="Judul harus diisi")
 buku_args.add_argument('deskripsi', type=str, required=True, help="Deskripsi harus diisi")
@@ -26,14 +28,43 @@ buku_args.add_argument('penulis', type=str, required=True, help="Penulis harus d
 buku_args.add_argument('genre', type=str, required=True, help="Genre harus diisi")
 buku_args.add_argument('tahun', type=int, required=True, help="Tahun harus diisi")
 
+# Format Response (GET JSON)
+buku_fields = {
+    'id': fields.Integer,
+    'judul': fields.String,
+    'deskripsi': fields.String,
+    'penulis': fields.String,
+    'genre': fields.String,
+    'tahun': fields.Integer
+}
+
 class Buku(Resource):
+    # GET Response data buku (dalam bentuk JSON)
+    @marshal_with(buku_fields)
     def get(self):
         buku = BukuModel.query.all()
         return buku
+    
+    # POST Data kedalam Database
+    @marshal_with(buku_fields)
+    def post(self):
+        args = buku_args.parse_args()
+        buku_baru = BukuModel(
+            judul=args["judul"],
+            deskripsi=args["deskripsi"],
+            penulis=args["penulis"],
+            genre=args["genre"],
+            tahun=args["tahun"])
+        try: 
+            db.session.add(buku_baru)
+            db.session.commit()
+            return buku_baru, 201
+        except SQLAlchemyError as err:
+            db.session.rollback()
+            abort(400, message=f"Kesalahan pada Database {err}")
 
 def conn_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
     db.init_app(app)
     with app.app_context():
         db.create_all()
-
